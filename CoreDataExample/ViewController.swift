ae2846f629aecd1e7d8a1965b856320e4f015150
mahmutmercan <56167628+mahmutmercan@users.noreply.github.com>
2021-08-09 10:21:42 +0300

    First

---
+++ CoreDataExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataExample
+//
+//  Created by Mahmut MERCAN on 9.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
