14d310396ead19b2e05bbae08ee15fc466f82bde
mahmutmercan <56167628+mahmutmercan@users.noreply.github.com>
2021-08-09 12:02:09 +0300

    LastCommit

--- CoreDataExample/ViewController.swift
+++ CoreDataExample/ViewController.swift
@@ -24,6 +24,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Do any additional setup after loading the view.
         title = "CoreData To Do List"
         view.addSubview(tableView)
+        getAllItems()
         tableView.delegate = self
         tableView.dataSource = self
         tableView.frame = view.bounds
@@ -32,7 +33,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     @objc private func didTapAdd() {
-        
+        let alert = UIAlertController(title: "New Item", message: "Enter New Item", preferredStyle: .alert)
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(UIAlertAction(title: "Submit", style: .cancel, handler: { [weak self] _ in
+            guard let field = alert.textFields?.first, let text = field.text, !text.isEmpty else {
+                return
+            }
+            self?.createItem(name: text)
+        }))
+        present(alert, animated: true)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -44,6 +53,40 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = model.name
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let item = models[indexPath.row]
+        
+        let sheet = UIAlertController(title: "Edit", message: nil, preferredStyle: .actionSheet)
+        
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        
+        
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: { _ in
+            
+            let alert = UIAlertController(title: "Edit Item", message: "Edit Your Item", preferredStyle: .alert)
+            alert.addTextField(configurationHandler: nil)
+            alert.textFields?.first?.text = item.name
+            alert.addAction(UIAlertAction(title: "Save", style: .cancel, handler: { [weak self] _ in
+                guard let field = alert.textFields?.first, let newName = field.text, !newName.isEmpty else {
+                    return
+                }
+                self?.updateItem(item: item, newName: newName)
+            }))
+            self.present(alert, animated: true)
+
+        }))
+        
+        
+        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { [weak self] _ in
+            self?.deleteItem(item: item)
+        }))
+
+        present(sheet, animated: true)
     }
 
     
@@ -67,6 +110,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         do {
             try context.save()
+            getAllItems()
         }
         catch {
             
@@ -77,6 +121,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         context.delete(item)
         do {
             try context.save()
+            getAllItems()
         }
         catch {
             
@@ -90,6 +135,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         do {
             try context.save()
+            getAllItems()
         }
         catch {
             
