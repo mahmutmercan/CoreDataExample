c7fb2d5273103ec0c34de28b9d463eda7307c607
mahmutmercan <56167628+mahmutmercan@users.noreply.github.com>
2021-08-09 11:24:20 +0300

    2.

---
+++ CoreDataExample/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  CoreDataExample
+//
+//  Created by Mahmut MERCAN on 9.08.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
